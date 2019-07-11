7ae3ce9707da6ae971ca641f1eb4298e7dcb5828
Jaron Rosenau <jaron.rosenau@gmail.com>
2019-07-10 19:24:40 -0700

    Update testnetTokenList.ts

--- src/contracts/testnetTokenList.ts
+++ src/contracts/testnetTokenList.ts
@@ -1,8 +1,6 @@
 import QRCToken from '../models/QRCToken';
 
 const testnetTokenList: QRCToken[] = [
-  new QRCToken('Prediction Token', 'PRED', 8, 'a7489701415b46bdbd7e91dc5164cb50ae5dfe8f'),
-  new QRCToken('Fun Token', 'FUN', 8, '33c3c5736bc8b1f04d074f44568c350fe94e865e'),
 ];
 
 export default testnetTokenList;
