f351a689402cb9d95d5567b6cf53b703289af64c
Jaron Rosenau <jaron.rosenau@gmail.com>
2019-07-10 18:58:47 -0700

    Update externalController.ts

--- src/background/controllers/externalController.ts
+++ src/background/controllers/externalController.ts
@@ -1,37 +1,37 @@
 import axios from 'axios';
 
-import RunebaseChromeController from '.';
+import HtmlcoinChromeController from '.';
 import IController from './iController';
 import { MESSAGE_TYPE } from '../../constants';
 
 const INIT_VALUES = {
   getPriceInterval: undefined,
-  runebasePriceUSD: 0,
+  htmlcoinPriceUSD: 0,
 };
 
 export default class ExternalController extends IController {
   private static GET_PRICE_INTERVAL_MS: number = 60000;
 
   private getPriceInterval?: number = INIT_VALUES.getPriceInterval;
-  private runebasePriceUSD: number = INIT_VALUES.runebasePriceUSD;
+  private htmlcoinPriceUSD: number = INIT_VALUES.htmlcoinPriceUSD;
 
-  constructor(main: RunebaseChromeController) {
+  constructor(main: HtmlcoinChromeController) {
     super('external', main);
     this.initFinished();
   }
 
-  public calculateRunebaseToUSD = (balance: number): number => {
-    return this.runebasePriceUSD ? Number((this.runebasePriceUSD * balance).toFixed(2)) : 0;
+  public calculateHtmlcoinToUSD = (balance: number): number => {
+    return this.htmlcoinPriceUSD ? Number((this.htmlcoinPriceUSD * balance).toFixed(2)) : 0;
   }
 
   /*
   * Starts polling for periodic info updates.
   */
   public startPolling = async () => {
-    await this.getRunebasePrice();
+    await this.getHtmlcoinPrice();
     if (!this.getPriceInterval) {
       this.getPriceInterval = window.setInterval(() => {
-        this.getRunebasePrice();
+        this.getHtmlcoinPrice();
       }, ExternalController.GET_PRICE_INTERVAL_MS);
     }
   }
@@ -47,25 +47,25 @@ export default class ExternalController extends IController {
   }
 
   /*
-  * Gets the current Runebase market price.
+  * Gets the current Htmlcoin market price.
   */
-  private getRunebasePrice = async () => {
+  private getHtmlcoinPrice = async () => {
     try {
       // const jsonObj = await axios.get('https://api.coinmarketcap.com/v2/ticker/xxxx/');
-      const jsonObj = await axios.get('https://api.coinpaprika.com/v1/ticker/runes-runebase');
-      // this.runebasePriceUSD = jsonObj.data.data.quotes.USD.price;
-      this.runebasePriceUSD = jsonObj.data.price_usd;
+      const jsonObj = await axios.get('https://api.coinpaprika.com/v1/ticker/html-htmlcoin');
+      // this.htmlcoinPriceUSD = jsonObj.data.data.quotes.USD.price;
+      this.htmlcoinPriceUSD = jsonObj.data.price_usd;
 
       if (this.main.account.loggedInAccount
         && this.main.account.loggedInAccount.wallet
         && this.main.account.loggedInAccount.wallet.info
       ) {
-        const runebaseUSD = this.calculateRunebaseToUSD(this.main.account.loggedInAccount.wallet.info.balance);
-        this.main.account.loggedInAccount.wallet.runebaseUSD = runebaseUSD;
+        const htmlcoinUSD = this.calculateHtmlcoinToUSD(this.main.account.loggedInAccount.wallet.info.balance);
+        this.main.account.loggedInAccount.wallet.htmlcoinUSD = htmlcoinUSD;
 
         chrome.runtime.sendMessage({
-          type: MESSAGE_TYPE.GET_RUNEBASE_USD_RETURN,
-          runebaseUSD,
+          type: MESSAGE_TYPE.GET_HTMLCOIN_USD_RETURN,
+          htmlcoinUSD,
         });
       }
     } catch (err) {
