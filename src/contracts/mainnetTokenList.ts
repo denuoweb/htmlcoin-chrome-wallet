e33cde64b42d266cf734105d08ca9f615401a173
Jaron Rosenau <jaron.rosenau@gmail.com>
2019-07-10 19:17:02 -0700

    Update mainnetTokenList.ts

--- src/contracts/mainnetTokenList.ts
+++ src/contracts/mainnetTokenList.ts
@@ -1,8 +1,17 @@
 import QRCToken from '../models/QRCToken';
 
 const mainnetTokenList: QRCToken[] = [
-  new QRCToken('Prediction Token', 'PRED', 8, 'd271c668dbb500e0567ea2c18f6525da9443d7d4'),
-  new QRCToken('Fun Token', 'Fun', 8, '84704b205c32d602bdb18a4fe14eb4dffc9cd10d'),
+  new QRCToken('HTMLBunker', 'BUNK', 8, '6e7c1636f021563e0c95b134f55c3d48c63d5051'),
+  new QRCToken('XOLA', 'XOLA', 8, '567190cb97dfe363342fcf178e52d67040388dad'),
+  new QRCToken('CrypticMAG', 'MAG', 8, '8ef863bce3568898f293596b4638d00876bada86'),
+  new QRCToken('ROY', 'ROY', 8, 'aff123aab8e8e2f02ed07e366b18e9a5f2479d32'),
+  new QRCToken('Auto Sales Wear', 'ASW', 8, '71c2d38b323aa33528cf5589de7cf4ceb9bd7125'),
+  new QRCToken('Biffy Diamond', 'BIFD', 8, 'e659893bc750173b626d53a94b3f728b09742733'),
+  new QRCToken('MyTeenCoin', 'TEEN', 8, '8ced6e2d3beafcb5d0696d3651faf2a6cd68263a'),
+  new QRCToken('Connections By Design', 'CBD', 8, '1dd107453be5f5b97ce861d896269407a4e8ec21'),
+  new QRCToken('NEWS Token Pool', 'NETP', 8, 'e08e0dafd1efef65b8dc9c139dfd54ca409c740e'),
+  new QRCToken('GAMES', 'GAMES', 8, '9a4e8fe4f7d83fa52002e40c5792ca260847c323'),
+  new QRCToken('CSS', 'CSS', 8, 'e2f2be4bd96b331fe524455c4b81bd125653bbc0'),
 ];
 
 export default mainnetTokenList;
